aaedb604dfaa04ea07b7b24f560cace52f0852f7
josiahruddell <jruddell@gmail.com>
2012-02-13 02:14:50 -0600

    firefox fix

--- public/javascripts/util/data.js
+++ public/javascripts/util/data.js
@@ -1,7 +1,7 @@
 define(['jquery', 'lib/socket.io/socket.io.min'], function($){
     var socket = io.connect(document.domain, {
         reconnect: true,
-        transports: ['websocket', 'htmlfile', 'xhr-multipart', 'xhr-polling', 'jsonp-polling'] //websocket is not working with joyent hosting
+        transports: [/*'websocket', */'htmlfile', 'xhr-multipart', 'xhr-polling', 'jsonp-polling'] //websocket is not working with joyent hosting
     });
     
     var storage = sessionStorage || localStorage;
